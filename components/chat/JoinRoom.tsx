2f53ce327bba52a3beabcbf411f564fb778b6a56
Pouria Darandi <p.darandi@asredanesh.local>
2025-02-03 09:34:01 +0330

    improved join room form

--- components/chat/JoinRoom.tsx
+++ components/chat/JoinRoom.tsx
@@ -17,24 +17,35 @@ const JoinRoom = (props: JoinRoomProps) => {
     };
 
     return (
-        <div className="flex flex-col items-center max-w-3xl mx-auto w-full">
-            <h1 className="text-2xl font-bold mb-4">Join a room</h1>
-            <form onSubmit={handleJoinRoom} className="flex flex-col gap-2">
+        <div className="flex flex-col items-center max-w-64 mx-auto space-y-4 w-ful">
+            <div className="text-center space-y-1">
+                <h1 className="text-3xl font-bold">Join a room</h1>
+                <p className="text-zinc-500">
+                    fill out the form below to join a room
+                </p>
+            </div>
+            <form
+                onSubmit={handleJoinRoom}
+                className="flex flex-col gap-2.5 w-full">
                 <input
+                    min={1}
+                    required
                     type="text"
                     name="username"
                     value={username}
-                    placeholder="Username"
+                    placeholder="nickname"
                     className="flex-1 rounded-lg text-zinc-700
                     selection:bg-zinc-300 selection:text-zinc-700
                     border-2 focus:border-zinc-400 focus:outline-none px-4 py-2"
                     onChange={(event) => setUsername(event.target.value)}
                 />
                 <input
+                    min={1}
+                    required
                     type="text"
                     name="roomCode"
                     value={roomCode}
-                    placeholder="Room Code"
+                    placeholder="room code"
                     className="flex-1 rounded-lg
                     text-zinc-700 selection:bg-zinc-300 selection:text-zinc-700
                     border-2 focus:border-zinc-400 focus:outline-none px-4 py-2"
